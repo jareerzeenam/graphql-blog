bfafee3641a448506a68bd7f6b8b5335d13fb6b8
jareer <jareerzeenam@adventus.io>
2022-11-04 11:57:09 +0530

    Unit test: Blog services initial commit

---
+++ tests/services/blogs.test.js
@@ -0,0 +1,11 @@
+// TODO ::
+
+describe('services/blogs', () => {
+  describe('createBlog()', () => {
+    test('should create a blog', async () => {
+      const test = 1;
+
+      expect(test).toBe(1);
+    });
+  });
+});
